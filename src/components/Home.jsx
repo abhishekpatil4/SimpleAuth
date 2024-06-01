dbf5e4686a5e54d55df7586fc9a2a303931558af
Abhishek Patil <abishkpatil@gmail.com>
2024-06-01 20:36:25 +0530

    feat: modified home component header color

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,19 @@ import useMediaQuery from './useMediaQuery';
 
 const Home = () => {
     const isSmallScreen = useMediaQuery('(max-width: 400px)');
-    return <div style={{margin: isSmallScreen ? '50% 0%' : '10% 0%'}}>
-        <h1 style={{lineHeight:'4rem'}}>Simple React app that uses <br /> <span style={{color:'#FFC402'}}>Firebase</span> for <span style={{color:'#DD2C01'}}>Authentiation</span></h1>
+    return <div style={{ margin: isSmallScreen ? '50% 0%' : '10% 0%' }}>
+        <h1 style={{ lineHeight: '4rem' }}>Simple React app that uses <br />
+            <span style={{
+                background: 'linear-gradient(to right, #FFC402, #DD2C01)', WebkitTextFillColor: 'transparent',
+                WebkitBackgroundClip: 'text'
+            }}>
+                Firebase
+            </span> for <span style={{
+                background: 'linear-gradient(to right, #FFC402, #DD2C01)', WebkitTextFillColor: 'transparent',
+                WebkitBackgroundClip: 'text'
+            }}>
+                Authentiation
+            </span></h1>
     </div>
 }
 
