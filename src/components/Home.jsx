90227f0c73658fb5aef757a7c51c9bec27dd010d
Abhishek Patil <abishkpatil@gmail.com>
2024-06-01 19:54:19 +0530

    feat: add home component content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 const Home = () => {
-    return <h1>Home</h1>
+    return <div style={{margin:'10%'}}>
+        <h1 style={{lineHeight:'4rem'}}>Simple React app that uses <br /> <span style={{color:'#FFC402'}}>Firebase</span> for <span style={{color:'#DD2C01'}}>Authentiation</span></h1>
+    </div>
 }
 
 export default Home;
