e7cfd63028ec1eb7df21f2799c0f1120bd4cc718
Abhishek Patil <abishkpatil@gmail.com>
2024-06-01 19:39:32 +0530

    feat: add sign in/up button in navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
 const Navbar = () => {
-    return <h1>Navbar</h1>
+    return <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div>
+            <h2>SimpleAuth</h2>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly', gap: 10, alignItems: 'center' }}>
+            <button style={{ minWidth: '8rem' }}>Sign In</button>
+            <button style={{ minWidth: '8rem' }}>Sign Up</button>
+        </div>
+    </div>
 }
 
 export default Navbar;
