1c2f6fcb95449898811c48eeae06f4fec2019a04
Abhishek Patil <abishkpatil@gmail.com>
2024-06-01 20:17:02 +0530

    feat: made home component responvive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
+import useMediaQuery from './useMediaQuery';
+
 const Home = () => {
-    return <div style={{margin:'10%'}}>
+    const isSmallScreen = useMediaQuery('(max-width: 400px)');
+    return <div style={{margin: isSmallScreen ? '50% 0%' : '10% 0%'}}>
         <h1 style={{lineHeight:'4rem'}}>Simple React app that uses <br /> <span style={{color:'#FFC402'}}>Firebase</span> for <span style={{color:'#DD2C01'}}>Authentiation</span></h1>
     </div>
 }
