dd2a6913a342dea1ce0c03337234e4c5d0223a82
Abhishek Patil <abishkpatil@gmail.com>
2024-06-01 20:14:21 +0530

    feat: add login icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
+import { IoIosLogIn } from "react-icons/io";
+import { IoIosLogOut } from "react-icons/io";
+import useMediaQuery from './useMediaQuery';
+
 const Navbar = () => {
+    const isSmallScreen = useMediaQuery('(max-width: 400px)');
     return <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <div>
             <h2>SimpleAuth</h2>
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-evenly', gap: 10, alignItems: 'center' }}>
-            <button style={{ minWidth: '8rem' }}>Sign In</button>
-            <button style={{ minWidth: '8rem' }}>Sign Up</button>
+            <button style={{ minWidth: isSmallScreen ? '4rem' : '8rem' }}>
+                {
+                    isSmallScreen ?
+                        <IoIosLogIn fontSize={"1.4rem"} />
+                        :
+                        "Sign In"
+                }
+            </button>
         </div>
     </div>
 }
