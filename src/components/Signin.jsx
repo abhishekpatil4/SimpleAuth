aab614a48f7dae1147f8ca9ed4d03340cd485769
Abhishek Patil <abishkpatil@gmail.com>
2024-06-03 00:34:57 +0530

    feat: add form to signin component

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,5 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 const Signin = () => {
-    return <h1>Signin</h1>
+    const navigate = useNavigate();
+    const handleSignin = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const payload = Object.fromEntries(data);
+        console.log("payload: ", payload);
+    }
+    return <div style={{ display: 'flex', justifyContent: 'center', marginTop:"10rem" }}>
+        <form onSubmit={handleSignin} style={{ gap: 10, display: 'flex', justifyContent: "center", flexDirection: 'column' }}>
+            <input required name="email" style={{ minWidth: '20rem', minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="text" placeholder="Email" />
+            <input required name="password" style={{ minWidth: '20rem', minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="password" placeholder="Password" />
+            <button style={{margin:'1rem 0rem'}}>Sign in</button>
+            <span style={{ fontSize: '15px' }}>New? <span style={{ textDecoration: '1px underline', textUnderlineOffset: '0.25em', cursor: 'pointer' }} onClick={() => navigate('/signup')}>Sign up here</span></span>
+        </form>
+    </div>
 }
 
 export default Signin;
