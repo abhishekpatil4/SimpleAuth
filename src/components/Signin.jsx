e546d746819d424f2e1153e288027627ea5cc98b
Abhishek Patil <abishkpatil@gmail.com>
2024-06-03 00:41:03 +0530

    fix: minor bugs

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -8,11 +8,12 @@ const Signin = () => {
         const payload = Object.fromEntries(data);
         console.log("payload: ", payload);
     }
-    return <div style={{ display: 'flex', justifyContent: 'center', marginTop:"10rem" }}>
+    return <div style={{ maxWidth: '25rem', margin: 'auto' }}>
+        <h3>Sign into Your Account</h3>
         <form onSubmit={handleSignin} style={{ gap: 10, display: 'flex', justifyContent: "center", flexDirection: 'column' }}>
-            <input required name="email" style={{ minWidth: '20rem', minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="text" placeholder="Email" />
-            <input required name="password" style={{ minWidth: '20rem', minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="password" placeholder="Password" />
-            <button style={{margin:'1rem 0rem'}}>Sign in</button>
+            <input required name="email" style={{ minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="text" placeholder="Email" />
+            <input required name="password" style={{ minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="password" placeholder="Password" />
+            <button style={{ margin: '1rem 0rem' }}>Sign in</button>
             <span style={{ fontSize: '15px' }}>New? <span style={{ textDecoration: '1px underline', textUnderlineOffset: '0.25em', cursor: 'pointer' }} onClick={() => navigate('/signup')}>Sign up here</span></span>
         </form>
     </div>
