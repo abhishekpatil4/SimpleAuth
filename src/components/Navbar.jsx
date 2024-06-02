af840f12d95ab10c11a80a13df5582a375d131ce
Abhishek Patil <abishkpatil@gmail.com>
2024-06-03 00:14:36 +0530

    feat: add redirect function to signin button and conditionally rendering based on pathname using useLocation(), minor refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,19 @@ import { IoIosLogIn } from "react-icons/io";
 import { IoIosLogOut } from "react-icons/io";
 import useMediaQuery from './useMediaQuery';
 import { Link } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const isSmallScreen = useMediaQuery('(max-width: 400px)');
     return <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <div onClick={() => navigate('/')} style={{ cursor: 'pointer' }}>
             <h2>SimpleAuth</h2>
         </div>
-        <div style={{ display: 'flex', justifyContent: 'space-evenly', gap: 10, alignItems: 'center' }}>
-            <button style={{ minWidth: isSmallScreen ? '4rem' : '8rem' }}>
+        {
+            location.pathname !== "/signin" &&
+            <button onClick={() => navigate('/signin')} style={{ minWidth: isSmallScreen ? '4rem' : '8rem' }}>
                 {
                     isSmallScreen ?
                         <IoIosLogIn fontSize={"1.4rem"} />
@@ -20,7 +22,7 @@ const Navbar = () => {
                         "Sign In"
                 }
             </button>
-        </div>
+        }
     </div>
 }
 
