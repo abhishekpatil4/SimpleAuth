f5c42c316a199bed5160b7d26e5ffe86c29575ed
Abhishek Patil <abishkpatil@gmail.com>
2024-06-03 11:33:41 +0530

    feat: add support to login/signup with google account

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,14 +1,39 @@
 import { useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../config/Firebase";
 import { useState } from "react";
 import { Audio } from "react-loader-spinner";
 import { useSnackbar } from "notistack";
+import { FcGoogle } from "react-icons/fc";
 
 const Signin = () => {
     const navigate = useNavigate();
     const [signingIn, setSigningIn] = useState(false);
     const { enqueueSnackbar } = useSnackbar();
+    const [googleSigningUp, setGoogleSigningUp] = useState(false);
+    const handleGoogleSignin = async () => {
+        setGoogleSigningUp(true);
+        const provider = new GoogleAuthProvider();
+        try {
+            await signInWithPopup(auth, provider);
+            enqueueSnackbar("Signed In", {
+                variant: 'success', anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'center'
+                }
+            })
+            setGoogleSigningUp(false);
+            navigate('/');
+        } catch (error) {
+            enqueueSnackbar(error.message, {
+                variant: 'error', anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'center'
+                }
+            })
+            setGoogleSigningUp(false);
+        }
+    }
     const handleSignin = async (event) => {
         setSigningIn(true);
         event.preventDefault();
@@ -39,7 +64,7 @@ const Signin = () => {
         <form onSubmit={handleSignin} style={{ gap: 10, display: 'flex', justifyContent: "center", flexDirection: 'column' }}>
             <input required name="email" style={{ minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="text" placeholder="Email" />
             <input required name="password" style={{ minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="password" placeholder="Password" />
-            <button style={{ margin: '1rem 0rem' }}>
+            <button type="submit" style={{ margin: '1rem 0rem 0rem 0rem' }}>
                 {
                     signingIn ?
                         <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -55,6 +80,24 @@ const Signin = () => {
                         </div>
                         :
                         "Sign in"
+                }
+            </button>
+            <button onClick={handleGoogleSignin} type="button">
+                {
+                    googleSigningUp ?
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Audio
+                                height="15"
+                                width="15"
+                                radius="9"
+                                color="white"
+                                ariaLabel="loading"
+                                wrapperStyle
+                                wrapperClass
+                            />
+                        </div>
+                        :
+                        <div style={{ display: "flex", alignItems: 'center', justifyContent: 'center', gap: 8 }}><FcGoogle /> Google</div>
                 }
             </button>
             <span style={{ fontSize: '15px' }}>New? <span style={{ textDecoration: '1px underline', textUnderlineOffset: '0.25em', cursor: 'pointer' }} onClick={() => navigate('/signup')}>Sign up here</span></span>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,14 +1,39 @@
 import { useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../config/Firebase";
 import { useState } from "react";
 import { Audio } from 'react-loader-spinner'
 import { useSnackbar } from "notistack";
+import { FcGoogle } from "react-icons/fc";
 
 const Signup = () => {
     const navigate = useNavigate();
     const [signingUp, setSigningUp] = useState(false);
+    const [googleSigningUp, setGoogleSigningUp] = useState(false);
     const { enqueueSnackbar } = useSnackbar();
+    const handleGoogleSignin = async () => {
+        setGoogleSigningUp(true);
+        const provider = new GoogleAuthProvider();
+        try {
+            await signInWithPopup(auth, provider);
+            enqueueSnackbar("Account Created", {
+                variant: 'success', anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'center'
+                }
+            })
+            setGoogleSigningUp(false);
+            navigate('/');
+        } catch (error) {
+            enqueueSnackbar(error.message, {
+                variant: 'error', anchorOrigin: {
+                    vertical: 'bottom',
+                    horizontal: 'center'
+                }
+            })
+            setGoogleSigningUp(false);
+        }
+    }
     const handleSignin = async (event) => {
         setSigningUp(true);
         event.preventDefault();
@@ -49,7 +74,7 @@ const Signup = () => {
             <input required name="email" style={{ minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="text" placeholder="Email" />
             <input required name="password" style={{ minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="password" placeholder="Password" />
             <input required name="password2" style={{ minHeight: '2.8rem', paddingLeft: '0.8rem', borderRadius: '8px', border: '0px' }} type="password" placeholder="Confirm Password" />
-            <button style={{ margin: '1rem 0rem' }}>
+            <button type="submit" style={{ margin: '1rem 0rem 0rem 0rem' }}>
                 {signingUp ?
                     <div style={{ display: 'flex', justifyContent: 'center' }}>
                         <Audio
@@ -64,6 +89,24 @@ const Signup = () => {
                     </div>
                     :
                     "Sign up"
+                }
+            </button>
+            <button onClick={handleGoogleSignin} type="button">
+                {
+                    googleSigningUp ?
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Audio
+                                height="15"
+                                width="15"
+                                radius="9"
+                                color="white"
+                                ariaLabel="loading"
+                                wrapperStyle
+                                wrapperClass
+                            />
+                        </div>
+                        :
+                        <div style={{ display: "flex", alignItems: 'center', justifyContent: 'center', gap: 8 }}><FcGoogle /> Google</div>
                 }
             </button>
             <span style={{ fontSize: '15px' }}>Already have an account? <span style={{ textDecoration: '1px underline', textUnderlineOffset: '0.25em', cursor: 'pointer' }} onClick={() => navigate('/signin')}>Sign in here</span></span>
