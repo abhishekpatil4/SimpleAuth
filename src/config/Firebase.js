f742c589edcfb574f02005bf6829cc3ff40d9128
Abhishek Patil <abishkpatil@gmail.com>
2024-06-03 10:14:53 +0530

    fix: moved firebase config values to .env file

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCe63qTxqb3XkRI5zkghUbARYVtJ8FI74w",
-  authDomain: "fb-auth-e06b5.firebaseapp.com",
-  projectId: "fb-auth-e06b5",
-  storageBucket: "fb-auth-e06b5.appspot.com",
-  messagingSenderId: "71093994251",
-  appId: "1:71093994251:web:53ba811002d9f35c32980e",
-  measurementId: "G-19ESFEJXZ9"
+    apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+    authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+    projectId: import.meta.env.VITE_FIREBASE_projectId,
+    storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+    appId: import.meta.env.VITE_FIREBASE_appId,
+    measurementId: import.meta.env.VITE_FIREBASE_measurementId
 };
 
 const app = initializeApp(firebaseConfig);
