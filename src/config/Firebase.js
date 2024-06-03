92da2f56c95fd62c540063c9054b20f77d4a93d5
Abhishek Patil <abishkpatil@gmail.com>
2024-06-03 07:44:12 +0530

    feat: add firebase

---
+++ src/config/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCe63qTxqb3XkRI5zkghUbARYVtJ8FI74w",
+  authDomain: "fb-auth-e06b5.firebaseapp.com",
+  projectId: "fb-auth-e06b5",
+  storageBucket: "fb-auth-e06b5.appspot.com",
+  messagingSenderId: "71093994251",
+  appId: "1:71093994251:web:53ba811002d9f35c32980e",
+  measurementId: "G-19ESFEJXZ9"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
